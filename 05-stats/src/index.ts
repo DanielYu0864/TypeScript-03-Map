06c4805f73219946557a64a7760c437916477801
Daniel.Yu <adam741963@gmail.com>
2021-01-28 19:25:21 -0800

    first setup with console reader and win analysis

--- 05-stats/src/index.ts
+++ 05-stats/src/index.ts
@@ -1,16 +1,23 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalys';
+import { Summary } from './Summary';
 //? step to get data from csv file
 //? Load (node std lib) -> prase -> analyze -> report
 
 //step 1 Create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader('football.csv');
+
 //step 2 Create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-// matchReader.matches
+//EX matchReader.matches
 
+//step 3 call data
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  new ConsoleReport()
+);
 
-
-console.log('Man United won ' + manUnitedWins + ' games.');
+summary.buildAndPrintReport(matchReader.matches);
